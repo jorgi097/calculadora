bb2921fe39c8e416e684f29c57d709b3dbd1305f
jorge garcia <jorge.garcia.celis@gmail.com>
2024-04-29 14:41:21 -0600

    final

--- src/components/Boton.js
+++ src/components/Boton.js
@@ -1,16 +1,15 @@
 import React from "react";
 import '../styles/boton.css'
-import { useState } from "react";
 
 function Boton(props){
     const esOperador = (valor) => {
-        return isNaN(valor) && (valor != '.') && (valor != '=')
+        return isNaN(valor) && (valor !== '.') && (valor !== '=')
     }
 
-
-
     return(
-      <div className={`boton ${esOperador(props.children) ? 'operador': ''}`.trimEnd()}>
+      <div
+        className={`boton ${esOperador(props.children) ? 'operador': ''}`.trimEnd()}
+        onClick={() => props.manejarClic(props.children)}>
         {props.children}
       </div>  
     )
